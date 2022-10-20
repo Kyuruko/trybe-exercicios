a7acd2d4611519566a1e93deeca241177217dd0f
bruno <bruno.costa1203@yahoo.com>
2022-10-20 14:53:00 -0300

    Exercicios da aula

---
+++ fundamentos/secao-04-introducao-a-javascript/Dia-1-JavaScript-Primeiros-Passos/script.js
@@ -0,0 +1,81 @@
+// let myAge = 25;
+// let myName = "Bruno";
+// let birthCity = "Rio Grande";
+// let birthYear = 1997;
+
+// console.log("Meu nome é " +myName+" nasci em "+birthCity+" no ano de "+birthYear)
+                                                        // --//--
+
+// const base = 5;
+// const height = 8;
+// let area;
+// let perimeter;
+
+// console.log(perimeter=(base*2)+(height*2))
+
+// console.log(area = base*height)
+                                                        // --//--
+
+// let n = Math.round(Math.random(0,100)*100);
+// console.log(n)
+
+// if(n >= 80){
+//     console.log("Aprovado")
+// }
+// else if(n >= 60 && n < 80){
+//     console.log("Lista de Espera")
+// }
+// else{
+//     console.log("Reprovado")
+// }
+                                                        // --//--
+// let currentHour = 22;
+// let message;
+// console.log(currentHour)
+// if(currentHour >= 22){
+//     let message = "Não deveríamos comer nada, é hora de dormir";
+//     console.log(message)
+// }
+// else if(currentHour >= 18 && currentHour < 22){
+//     let message = "Rango da noite, vamos jantar :D"
+//     console.log(message)
+// }
+// else if(currentHour >= 11 && currentHour <= 14){
+//     let message = "Hora do almoço!!!"
+//     console.log(message)
+// }
+// else if(currentHour >= 4 && currentHour <= 11){
+//     let message = "Hmmm, cheiro de café recém-passado"
+//     console.log(message)
+// }
+                                                        // --//--
+// let weekDay = "sab"
+
+// if(weekDay == "seg" || weekDay == "ter" || weekDay == "qua" || weekDay == "qui" || weekDay == "sex"){
+//     console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+// }else{
+//     console.log("FINALMENTE, descanso merecido UwU")
+// }
+                                                        // --//--
+
+// let situation = "aprovada"
+
+// switch(situation){
+//     case("aprovada"):
+
+//         console.log("Parabéns, você foi aprovada(o)!");
+//         break
+    
+//     case("lista"):
+
+//         console.log("Você está na nossa lista de espera");
+//         break
+
+//     case("reprovada"):
+
+//         console.log("Você foi reprovada(o)");
+//         break
+//     default:
+//         console.log("Informação incorreta")
+
+// }
