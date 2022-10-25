358071434ec2f164fb7a58c6594e686990493758
bruno <bruno.costa1203@yahoo.com>
2022-10-25 14:46:16 -0300

    Exercicios de OO

---
+++ fundamentos/secao-04-introducao-a-javascript/Dia-4-Functions-e-OO/exer-OO.js
@@ -0,0 +1,62 @@
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+// let info2 = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+//   nota: "O último MacPatinhas"
+// }
+
+// info["recorrente"] = "Sim"
+// info2["recorrente"] = "Sim"
+
+
+// for (i in info){
+  // console.log(i)
+  // console.log(info[i])
+// }
+
+// console.log(`${info.personagem} e ${info2.personagem}`)
+// console.log(`${info.origem} e ${info2.origem}`)
+// console.log(`${info.nota} e ${info2.nota}`)
+// if(info.recorrente == info2.recorrente){
+//   console.log("Ambos recorrentes")
+// }else{
+//   console.log("Algum não recorrente")
+// }
+
+
+// console.log(info.recorrente)
+// console.log(`Nome da personagem: ${info.personagem}, origem dos quadrinhos do: ${info.origem}, ela é ${info.nota}`)
+
+
+
+// let cont = 0
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
+// leitor.livrosFavoritos[0] = {
+//   cont: 1,
+// }
+// leitor.livrosFavoritos[1] =  {
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+//   cont: 1,
+// }
+// cont = leitor.livrosFavoritos[1].cont + leitor.livrosFavoritos[0].cont
+// console.log(`${leitor.nome} e possui ${cont} livros favoritos`)
+// console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} é '${leitor.livrosFavoritos[1].titulo}'.`)
+
+
