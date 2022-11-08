986f6880124e3e7e5a82ec482a9328c7b8c8e539
bruno <bruno.costa1203@yahoo.com>
2022-11-08 19:50:58 -0300

    exercicios completos

--- fundamentos/secao-06-Flexbox-Responsividade/Dia-01-Forms/script.js
+++ fundamentos/secao-06-Flexbox-Responsividade/Dia-01-Forms/script.js
@@ -1,21 +1,37 @@
-const submitBtn = document.querySelector('.submitForm')
-const form = document.querySelector('#form')
-const clear = document.querySelector('.clear')
+const submitBtn = document.querySelector('.submitForm');
+const form = document.querySelector('#form');
+const clear = document.querySelector('.clear');
+const checkboxChecked = document.querySelector("#inp-checkbox-1");
 
-submitBtn.addEventListener('click', (event)=>{
-    console.log('teste')
-    event.preventDefault()
-})
+submitBtn.addEventListener('click', handleSubmit)
 clear.addEventListener('click', ()=>{
-    form.reset()
+    form.reset();
 })
-
+checkboxChecked.addEventListener('change', isChecked)
 function isChecked(){
-    if(document.querySelector("#inp-checkbox-1").checked){
-        console.log('xablau')
-        submitBtn.addEventListener('click', ()=>{
-            form.submit()
-        })
+    const checkboxChecked = document.querySelector("#inp-checkbox-1");
+    submitBtn.disabled = !checkboxChecked.checked;
+}
+function invalidInput(){
+    const email = document.querySelector('#email').value.length;
+    const invalidEmail = email < 10 || email > 50;
+    const name = document.querySelector('#name').value.length;
+    const invalidName = name < 10 || name > 40;
+    const textArea = document.querySelector('#inp-textarea').value.length;
+    const invalidArea = textArea > 500;
+
+    if(invalidArea || invalidEmail || invalidName){
+        return false
+    } else {
+        return true
     }
 }
-isChecked()
+function handleSubmit(event) {
+    event.preventDefault();
+    const validation = textInputValidation();
+    if (validation === false) {
+      alert('Dados inválidos');
+    } else {
+      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
+  }
